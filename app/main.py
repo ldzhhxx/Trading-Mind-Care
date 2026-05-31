99f034abe32a181ce13bd4eb429f116e1f2ab435
ldzhhxx <ldzhhxx@users.noreply.github.com>
2026-06-01 00:58:59 +0800

    feat: 版本信息API端点

--- app/main.py
+++ app/main.py
@@ -76,3 +76,8 @@ app.mount("/static", StaticFiles(directory=static_dir), name="static")
 @app.get("/")
 async def index():
     return FileResponse(os.path.join(static_dir, "index.html"))
+
+
+@app.get("/api/version")
+async def version():
+    return {"version": "3.0.0", "name": "Trading Mind Care", "features": 42}
